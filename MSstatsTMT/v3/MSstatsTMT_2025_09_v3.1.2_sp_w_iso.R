2c77918b736b57caca9974dbb5e6773faac282e0
stephaniejh <stephaniejanehuang@gmail.com>
2025-09-09 18:19:56 +1200

    initial commit for MSstatsTMT v3.1.1 sp w iso R script

---
+++ MSstatsTMT/v3/MSstatsTMT_2025_09_v3.1.2_sp_w_iso.R
@@ -0,0 +1,12 @@
+#MSstatsTMT_2025_09_v3.1.2_sp_w_iso
+# 2025_09_09
+# stephaniejh
+
+# ~ * ` ~ * `
+# This script uses the PSMs from 2025_09_sp_w_iso which searched the runs in PD2.4
+# using the SwissProt db with isoforms (>9000) sequences. 
+# This MSstatsTMT analysis is used to compare results with the normal SwissProt (v3.1.1)
+# to 
+# & also
+
+# t3 - 
