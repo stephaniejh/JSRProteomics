bb6ecd6884a0aa848cfc92572a792a9e9ee5a651
stephaniejh <stephaniejanehuang@gmail.com>
2025-09-10 17:12:41 +1200

    set up v3.1.1.3 file (rerun with same settings as v3.1.2 t7 but using output from Nov 2024)

---
+++ MSstatsTMT/v3/MSstatsTMT_2025_09_v3.1.3_sp_w_iso_rrNov2024.R
@@ -0,0 +1,6 @@
+# MSstatsTMT_2025_09_v3.1.3_sp_w_iso_rerunNov2024.R
+
+# This pulls in the dataframe from Nov 2024 analysis sp w iso (9855 seq).
+# compared to v3.1.1.2 (sp w iso - 9878 seq) 
+
+# t1 
